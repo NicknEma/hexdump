fac3bceee593a3e9edefbf012dd93e8e96aac0d0
NicknEma <emanuele.rovini@gmail.com>
2025-05-24 12:46:49 +0200

    Improve control flow and error messages

--- hexdump.c
+++ hexdump.c
@@ -173,6 +173,9 @@ read_file_or_print_error(char *name) {
 
 #define DEFAULT_BYTES_PER_ROW 16
 
+#define     MAX_BYTES_PER_ROW 64
+#define     MIN_BYTES_PER_ROW  1
+
 #define INFO_TEXT \
 "Prints a hexadecimal view of one or more files.\n" \
 "Usage:\n" \
@@ -188,21 +191,22 @@ read_file_or_print_error(char *name) {
 
 int main(int argc, char **argv) {
 	
-	if (argc == 1) {
-		printf(INFO_TEXT);
+	bool args_ok = true;
+	
+	bool info_mode = false;
+	bool help_mode = false;
+	int  width = -1;
+	
+	int  file_count = 0;
+	
+	if (argc <= 1) { // Does it make sense to check for less-than? (Imagine a program invoking the process through API call passing the wrong arguments)
+		info_mode = true;
 	} else {
-		
-		bool args_ok = true;
-		
-		bool help_mode = false;
-		int  width = -1;
-		
-		int  file_count = 0;
-		
 		for (int argi = 1; argi < argc; argi += 1) {
 			String argn = string_from_cstring(argv[argi]);
-			if (argn.data[0] == '-') {
-				
+			if (argn.data[0] != '-') {
+				file_count += 1;
+			} else {
 				i64 sep_index = string_find_first(argn, ':');
 				String flag   = string_skip(string_chop_at(argn, sep_index >= 0 ? sep_index : argn.len), 1);
 				String rest   = string_skip(argn, sep_index >= 0 ? sep_index + 1 : argn.len);
@@ -214,143 +218,176 @@ int main(int argc, char **argv) {
 					// nothing else matters.
 					break;
 				} else if (string_equals(flag, string_from_lit("width"))) {
-					if (width == -1) {
-						if (rest.len > 0) {
+					if (width != -1) {
+						fprintf(stderr, "Ignoring repeated flag '-width'.\n");
+					} else if (rest.len > 0) {
+						
+						bool width_parsing_ok = true;
+						
+						char sign_char = ' ';
+						if (rest.data[0] == '+' || rest.data[0] == '-') {
+							sign_char = rest.data[0];
+							rest = string_skip(rest, 1);
 							
-							// TODO: Better control flow?
-							
-							bool all_digits = true;
+							if (rest.len == 0) {
+								fprintf(stderr, "The argument for '-width' must be an integer number, e.g. '-width:12'.\n");
+								width_parsing_ok = false;
+							}
+						}
+						
+						if (width_parsing_ok) {
 							for (int char_index = 0; char_index < rest.len; char_index += 1) {
 								if (!isdigit(rest.data[char_index])) {
-									all_digits = false;
+									fprintf(stderr, "The argument for '-width' must be an integer number, e.g. '-width:12'.\n");
+									width_parsing_ok = false;
 									break;
 								}
 							}
-							
-							if (all_digits) {
-								errno = 0;
-								int width_arg = atoi(rest.data);
-								if (errno == 0 && width_arg > 0) {
-									width = width_arg;
-								} else if (errno == ERANGE && width_arg == INT_MAX) {
-									fprintf(stderr, "Too thicc"); // TODO: Better error message
-									args_ok = false;
-									break;
+						}
+						
+						if (width_parsing_ok) {
+							if (sign_char == '-') {
+								fprintf(stderr, "The argument for '-width' must be a positive number, e.g. '-width:12'.\n");
+								width_parsing_ok = false;
+							}
+						}
+						
+						int width_arg = 0;
+						if (width_parsing_ok) {
+							for (int char_index = 0; char_index < rest.len; char_index += 1) {
+								char digit = rest.data[char_index] - '0';
+								if ((width_arg <= (MAX_BYTES_PER_ROW / 10)) &&
+									(width_arg <  (MAX_BYTES_PER_ROW / 10)) || digit <= (MAX_BYTES_PER_ROW % 10)) {
+									width_arg *= 10;
+									width_arg += digit;
 								} else {
-									fprintf(stderr, "Too smol"); // TODO: Better error message
-									args_ok = false;
+									fprintf(stderr, "The argument %.*s for '-width' is too big, the max supported is %d.\n", string_expand(rest), MAX_BYTES_PER_ROW);
+									width_parsing_ok = false;
 									break;
 								}
-							} else {
-								fprintf(stderr, "The argument for '-width' must be an integer number, e.g. '-width:12'.\n");
-								args_ok = false;
-								break;
 							}
+							if (width_arg < MIN_BYTES_PER_ROW) {
+								fprintf(stderr, "The argument %.*s for '-width' is too small, the min supported is %d.\n", string_expand(rest), MIN_BYTES_PER_ROW);
+								width_parsing_ok = false;
+							}
+						}
+						
+						if (width_parsing_ok) {
+							width = width_arg;
 						} else {
-							fprintf(stderr, "Flag '-width' requires an integer argument, e.g. '-width:12'.\n");
 							args_ok = false;
 							break;
 						}
+						
 					} else {
-						fprintf(stderr, "Ignoring repeated flag '-width'.\n");
+						fprintf(stderr, "Flag '-width' requires an integer argument, e.g. '-width:12'.\n");
+						args_ok = false;
+						break;
 					}
 				} else {
 					fprintf(stderr, "Ignoring unknown flag '-%.*s'. Try '-help' for a list of possible flags.\n", string_expand(flag)); // TODO: "Did you mean ...?"
 				}
-			} else {
-				file_count += 1;
 			}
 		}
+	}
+	
+	if (info_mode) {
+		printf(INFO_TEXT);
+	} else if (help_mode) {
+		printf(INFO_TEXT "\n" HELP_TEXT);
+	} else if (args_ok) {
+		// Main codepath: dump files.
 		
-		if (help_mode) {
-			printf(INFO_TEXT "\n" HELP_TEXT);
-		} else if (args_ok) {
-			// Main codepath: dump files.
+		if (width == -1) {
+			width = DEFAULT_BYTES_PER_ROW;
+		}
+		
+		assert(width > 0);
+		
+		if (file_count == 0) {
+			// No input files, but there were other arguments:
+			// It's an error.
 			
-			if (width == -1) {
-				width = DEFAULT_BYTES_PER_ROW;
+			fprintf(stderr, "No input files specified. Try again and specify at least one input file.\n");
+		} else {
+			char **file_names = malloc(file_count * sizeof(char *));
+			assert(file_names);
+			
+			for (int argi = 1, file_index = 0; argi < argc; argi += 1) {
+				char *argn = argv[argi];
+				
+				if (argn[0] != '-') {
+					file_names[file_index] = argn;
+					file_index += 1;
+				}
 			}
 			
-			assert(width > 0);
-			
-			if (file_count == 0) {
-				// No input files, but there were other arguments:
-				// It's an error.
+			for (int file_index = 0; file_index < file_count; file_index += 1) {
+				char *name = file_names[file_index];
+				printf("%s\n", name);
 				
-				fprintf(stderr, "No input files specified. Try again and specify at least one input file.\n");
-			} else {
-				char **file_names = malloc(file_count * sizeof(char *));
-				assert(file_names);
-				
-				for (int argi = 1, file_index = 0; argi < argc; argi += 1) {
-					char *argn = argv[argi];
+				Read_File_Result result = read_file_or_print_error(name);
+				if (result.valid) {
+					int bytes_per_row = width;
+					SliceU8 file = result.data;
 					
-					if (argn[0] != '-') {
-						file_names[file_index] = argn;
-						file_index += 1;
-					}
-				}
-				
-				for (int file_index = 0; file_index < file_count; file_index += 1) {
-					char *name = file_names[file_index];
-					printf("%s\n", name);
-					
-					Read_File_Result result = read_file_or_print_error(name);
-					if (result.valid) {
-						int bytes_per_row = width;
-						SliceU8 file = result.data;
+					for (i64 offset = 0; offset < file.len; offset += bytes_per_row) {
+						i64 eof    = min(offset + bytes_per_row, file.len);
+						i64 amount = eof - offset;
 						
-						for (i64 offset = 0; offset < file.len; offset += bytes_per_row) {
-							i64 eof    = min(offset + bytes_per_row, file.len);
-							i64 amount = eof - offset;
+						// The buffer needs to be big enough to hold:
+						//   8 + \t + (2*MAX_BYTES_PER_ROW + MAX_BYTES_PER_ROW-1) + \t + MAX_BYTES_PER_ROW
+						
+						char buffer[1024];
+						int  buffer_used = snprintf(buffer, array_count(buffer),
+													"%08llx\t", offset); // Print address
+						
+						{
+							// Print hex representation of the bytes
 							
-							char buffer[1024];
-							int  buffer_used = snprintf(buffer, array_count(buffer),
-														"%08llx\t", offset);
-							
-							{
-								for (i64 byte_index = 0; byte_index < amount; byte_index += 1) {
-									assert(offset + byte_index < file.len);
-									u8 b = file.data[offset + byte_index];
-									
-									buffer_used += snprintf(buffer + buffer_used, array_count(buffer) - buffer_used,
-															"%02x ", b);
-								}
+							for (i64 byte_index = 0; byte_index < amount; byte_index += 1) {
+								assert(offset + byte_index < file.len);
+								u8 b = file.data[offset + byte_index];
 								
-								for (i64 byte_index = amount; byte_index < bytes_per_row; byte_index += 1) {
-									buffer_used += snprintf(buffer + buffer_used, array_count(buffer) - buffer_used,
-															"   ");
+								buffer_used += snprintf(buffer + buffer_used, array_count(buffer) - buffer_used,
+														"%02x ", b);
+							}
+							
+							for (i64 byte_index = amount; byte_index < bytes_per_row; byte_index += 1) {
+								buffer_used += snprintf(buffer + buffer_used, array_count(buffer) - buffer_used,
+														"   ");
+							}
+							
+							buffer_used += snprintf(buffer + buffer_used, array_count(buffer) - buffer_used,
+													"\t");
+						}
+						
+						{
+							// Print ASCII representation of the bytes
+							
+							for (i64 byte_index = 0; byte_index < amount; byte_index += 1) {
+								assert(offset + byte_index < file.len);
+								u8 b = file.data[offset + byte_index];
+								
+								if (!isprint(b)) {
+									b = ' ';
 								}
 								
 								buffer_used += snprintf(buffer + buffer_used, array_count(buffer) - buffer_used,
-														"\t");
+														"%c", b);
 							}
-							
-							{
-								for (i64 byte_index = 0; byte_index < amount; byte_index += 1) {
-									assert(offset + byte_index < file.len);
-									u8 b = file.data[offset + byte_index];
-									
-									if (!isprint(b)) {
-										b = ' ';
-									}
-									
-									buffer_used += snprintf(buffer + buffer_used, array_count(buffer) - buffer_used,
-															"%c", b);
-								}
-							}
-							
-							printf("%.*s\n", cast(int) array_count(buffer), buffer);
 						}
 						
-						free(result.data.data);
+						printf("%.*s\n", cast(int) array_count(buffer), buffer);
 					}
 					
-					printf("\n");
+					free(result.data.data);
 				}
 				
-				// free(file_names);
+				printf("\n");
 			}
+			
+			// free(file_names);
 		}
 	}
 	
